3da1cab73cc887018f41bd8af84779516471ded0
ykawai6581 <103983379+ykawai6581@users.noreply.github.com>
2022-12-06 14:58:00 +0900

    Add files via upload

---
+++ obslog_generator.py
@@ -0,0 +1,301 @@
+import argparse
+import csv
+from datetime import datetime, timedelta
+import io
+import json
+import numpy as np
+import os
+import pandas as pd
+from past_observation_crawler import find_obsdates, find_weather_and_comments
+import requests
+import sys
+import tqdm
+import urllib
+
+parser = argparse.ArgumentParser(description=\
+'## obslog formatter ver. 2022 Nov. 19 ##')
+
+parser.add_argument('--obsdate', type=int, help='observation date in yymmdd format')
+parser.add_argument('--jd', help='if provided, timestamps will be given in jd instead of ut', action='store_true')
+parser.add_argument('--bypass', help='if provided, data download from wiki is bypassed. may be useful for checking tonight\'s obslog', action='store_true')
+parser.add_argument('--obj', type=str, help='if given, only the log for that object will be returned. \
+                                            variations of names (eg: TOI0XXXX and TOI-XXXX) are treated equally for most cases.\
+                                            planet identifiers (eg: -b, b, .01, .1 etc.) are not necessary because it is difficult to\
+                                            account for the inconsistencies and query may return empty results.\
+                                            case insensitive.',default='all')
+
+args = parser.parse_args()
+if args.obsdate is None and args.obj == 'all':
+   parser.error("at least one of --obsdate and --obj required")
+
+dirname = os.path.dirname(__file__)
+credentials = os.path.join(dirname, 'cred.json')
+server = os.path.join(dirname, 'ip.json')
+
+try: 
+    with open(credentials, 'r') as openfile:
+        payload = json.load(openfile)
+except FileNotFoundError:
+    username = input('username: ')
+    password = input('password: ')
+    payload = {"username": username, "password": password}
+
+try:
+    with open(server, 'r') as openfile:
+        ip = json.load(openfile)
+except FileNotFoundError:
+    ip = {"shine": "161.72.192.46"}
+
+#print(args.connect)
+# Use 'with' to ensure the session context is closed after use.
+if not args.bypass:
+    try:
+        with requests.Session() as s:
+            print('_____________________________________________\n')
+            print('Authenticating... (takes about 10-15 seconds)')
+            print('_____________________________________________')
+            p = s.post('http://research.iac.es/proyecto/muscat/users/login', data=payload)
+            # print the html returned or something more intelligent to see if it's a successful login page.
+            obs_path = 'http://research.iac.es/proyecto/muscat/observations/export'
+            targets_path = 'http://research.iac.es/proyecto/muscat/stars/export'
+            path_list = [obs_path,targets_path]
+            r = [s.get(path) for path in tqdm.tqdm(path_list, desc=f'Downloading past observation and registered targets data... (about 15 seconds)')]
+            obs_text = r[0].text#.encode('utf-8')
+            targets_text = r[1].text
+            #column = ["id","telescope","observer","weather","seeing","temperature","start_time","end_time","flats","bias","exposure_time","lightcurve","quicklook","comments","files","star_id","code"]
+            obs_reader = csv.reader(io.StringIO(obs_text), delimiter=';',quotechar='"',
+                                quoting=csv.QUOTE_ALL, skipinitialspace=True)#,index_col=column)
+            targets_reader = csv.reader(io.StringIO(targets_text), delimiter=';',quotechar='"',
+                                quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            observations_df = pd.DataFrame([row for row in obs_reader])
+            targets_df = pd.DataFrame([row for row in targets_reader])
+    except requests.exceptions.ConnectionError:
+        print('Bad connection with the server. A simple retry should fix the issue. Make sure that you are connected to VPN.')
+        sys.exit(1)
+if args.obsdate is None and args.obj != "all": #日付指定せず、かつ特定の天体を探している場合
+    obsdates, weather, comments = find_obsdates(args.obj,observations_df,targets_df)
+    #print(obsdates)
+else: #日付指定をしている場合(天体指定の有無に関わらず)weatherとコメントは後でprint_obslog内で取得するのでとりあえず空の箱を作る
+    obsdates = [str(args.obsdate)]
+    weather, comments = [""],[""]
+
+#returns the observation date according to input
+def obs(obsdate):
+    year = 2000 + int(obsdate[0:2])
+    month = int(obsdate[2:4])
+    day = int(obsdate[4:6])
+    return {'year':year,'month':month,'day':day}
+
+def past_midnight(int):
+    return int >= 0 and int <= 12
+
+#returns the time of an exposure as datetime object
+def round_ut(obsdate, x):
+    if int(x[0:2]) >= 0 and int(x[0:2]) <= 14:
+        obs_time = datetime(obs(obsdate)['year'],obs(obsdate)['month'],obs(obsdate)['day'],int(x[0:2]),int(x[3:5]),int(x[6:8])) + timedelta(days=1)
+    else:
+        obs_time = datetime(obs(obsdate)['year'],obs(obsdate)['month'],obs(obsdate)['day'],int(x[0:2]),int(x[3:5]),int(x[6:8]))
+    if int(x[6:8])  >= 58: #takes care of exposure times that have not been rounded up due to the microsecond lag between CCDs
+        #JDで下二桁clipをすると、10秒間なまされる
+        obs_time += timedelta(seconds=5)
+    return obs_time.replace(second=0, microsecond=0) 
+
+def round_jd(jd):
+    if len(str(int(jd))) == 4:
+        return float(str(jd)[0:8])
+    elif len(str(int(jd))) == 5:
+        return float(str(jd)[0:9])
+
+def unique(sequence):
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
+
+def adjust_name(str:str):
+    str = str.lower()
+    if 'toi0' in str:
+        str = str.replace('toi0', 'toi')
+    if '.01' in str:
+        str = str.replace('.01', '')
+    if '.1' in str:
+        str = str.replace('.1', '')
+    str = str.replace('-','')
+    return str
+
+def take_exposure_log(obslog, exptimes_list, exp_change_time_all_ccd, jd):
+    exptimes_mask = obslog.duplicated(subset=4)
+    exptimes = np.array(obslog)[~exptimes_mask]
+
+    exp_change_time = []
+    
+    if jd:
+        exp_change_time[:] = map(lambda x: round_jd(x), exptimes[:,3])
+    else:
+        exp_change_time[:] = map(lambda x: round_ut(obsdate, x), exptimes[:,4])
+
+    exp_change_time_all_ccd.append(exp_change_time)
+    exp_value = []
+    exp_value[:] = map(lambda x: f'{x}', exptimes[:,5])
+
+    exp_log = list(zip(exp_value,exp_change_time))
+    exptimes_list.append(exp_log)
+
+def take_focus_log (obslog,jd):
+    focus_mask = obslog.duplicated(subset=10)
+    focus = np.array(obslog)[~focus_mask]
+
+    focus_change_time = []
+    if jd:
+        focus_change_time[:] = map(lambda x: f'[{round_jd(x)}] ', focus[:,3])
+    else:
+        focus_change_time[:] = map(lambda x: f'[{x[0:5]}] ', focus[:,4])
+    focus_value = []
+    focus_value[:] = map(lambda x: f'{x}', focus[:,11])
+
+    focus_log = list(zip(focus_change_time,focus_value))
+    if len(focus_log) != 1:
+        focus_log[:] = map(lambda x: ''.join(x), focus_log)
+        focus_log = ', '.join(focus_log)
+    else:
+        focus_log = focus_log[0][1]
+    return focus_log
+
+def print_obslog(obsdate, obsdate_weather, comment, ip):
+    dt_object = datetime(obs(obsdate)['year'],obs(obsdate)['month'],obs(obsdate)['day'])
+    date_for_mail = dt_object.strftime('%B %d, %Y')
+
+    paths = []
+    for num in range(0,4):
+        paths.append(f'http://{ip}/obslog/{obsdate}/obslog-muscat2-{obsdate}-ccd{num}.csv')
+
+    active_ccds = list(range(0,4))
+    ccds = []
+    error_message = []
+    error_count = 0
+    for index, path in enumerate(tqdm.tqdm(paths, desc=f'\rDownloading obslog for observation on {date_for_mail}')):
+        try:
+            l  = np.array(pd.read_csv(path, encoding='UTF-8').reset_index())
+            ccds.append(l)
+        except urllib.error.HTTPError:
+            print(f'\r***No obslog is found for CCD {index} on {date_for_mail}***')
+            error_count += 1
+            if error_count == 4:
+                print(f'\r***There was no observation on {date_for_mail}***\r')
+            active_ccds.remove(index)
+        except pd.errors.ParserError:
+            print(f'\r***Obslog for CCD {index} may be corrupt. Please check on http://{ip}/obslog/{args.obsdate}/obslog-muscat2-{args.obsdate}-ccd{index}.html directly***\r')
+            active_ccds.remove(index)
+
+    if len(ccds) == 0:
+        sys.exit(1) 
+
+    if args.obj == "all":
+        object = unique(ccds[0][:,1])
+        try:
+            object.remove('FLAT')
+        except ValueError:
+            error_message.append('**MISSING FLAT** FLAT is NOT taken for this observation')
+            #pass
+        try:
+            object.remove('DARK')
+        except ValueError:
+            error_message.append('**MISSING DARK**DARK is NOT taken for this observation')
+            #pass
+    else: #create list of matching objects after removing '-'
+        object = [s for s in unique(ccds[0][:,1]) if adjust_name(args.obj) in adjust_name(s)]
+
+    df = pd.DataFrame(ccds[0])
+
+    print('\n====== OUTPUT ===================================\n')
+    print(f'[MuSCAT2] End of night report {date_for_mail}\n')
+    if len(object) == 1:
+        print(f'{object[0]} was observed on {date_for_mail}')
+    else:
+        print(f'{len(object)} targets were observed on {date_for_mail}')
+
+    for item in object:
+        obslog = df[df[1] == item].reset_index()
+        if args.jd:
+            obstimes = f'Obs: {obslog[2][0]} - {obslog[2].iloc[-1]} (JD)'
+        else:
+            obstimes = f'Obs: {obslog[3][0][0:5]} - {obslog[3].iloc[-1][0:5]} UT'
+
+        start_time = dt_object + timedelta(hours=int(obslog[3][0][0:5][:2])) + timedelta(minutes=int(obslog[3][0][0:5][3:]))
+        end_time = dt_object + timedelta(hours=int(obslog[3].iloc[-1][0:5][:2])) + timedelta(minutes=int(obslog[3].iloc[-1][0:5][3:]))
+        if past_midnight(int(obslog[3][0][0:5][:2])):
+            start_time += timedelta(days=1)
+        if past_midnight(int(obslog[3].iloc[-1][0:5][:2])):
+            end_time += timedelta(days=1)
+
+        exptimes_list = [] #list of tuples containing (exptime, change_time) for all exposures
+        exp_change_time_all_ccd = [] # cumulative list of all ccds of times when exposure time has changed
+
+        focus_log = take_focus_log(obslog,args.jd)
+        focus = "" #create empty string which can be overwritten in when generating obslog for emails and registeration to wiki
+        ag = None #create empty variable which can be overwritten in when generating obslog for emails and registeration to wiki
+
+        if args.obsdate is not None and not args.bypass: #if a specific date is returned and connected to wiki, obtain info from wiki
+            obsdate_weather, comment , focus , ag = find_weather_and_comments(item,observations_df,targets_df,obsdate,start_time,end_time,payload)
+               
+        for ccd in ccds:
+            df = pd.DataFrame(ccd)
+            obslog = df[df[1] == item].reset_index()
+            take_exposure_log(obslog,exptimes_list,exp_change_time_all_ccd,args.jd)
+
+        exp_change_time_all_ccd = sorted(unique(np.concatenate(exp_change_time_all_ccd)))
+
+        '''
+        prepare pandas dataframe containing the exposure times of each CCD at timestamps when exposure times were changed
+        eg:
+                0     1     2     3
+        20:21  15.0  90.0  90.0  90.0
+        20:32  90.0  90.0  90.0  90.0
+        20:45  30.0  90.0  90.0  90.0
+        20:56  30.0  90.0  15.0  90.0
+        '''
+        #initialize an empty dataframe
+        exp_df = pd.DataFrame(columns=range(len(active_ccds)),index=exp_change_time_all_ccd)#.sort_index()
+        #fill in cells
+        for index, exposure in enumerate(exptimes_list):
+            for log in unique(exposure):
+                exp_df[index][log[1]] = log[0]
+        exp_df = exp_df.fillna(method='ffill')
+        if not args.jd:
+            exp_df = exp_df.fillna(method='ffill').rename(index=lambda s: s.strftime('%H:%M'))
+        if ag is not None: #indicates the ccd used for ag if specified
+            active_ccds[active_ccds.index(ag)] = f'[{ag}]'
+        exp_df.columns = active_ccds
+        #if not simply write in a single line
+        if len(exp_df) == 1:
+            if ag is not None:
+                (exp_df.iloc[0][ag]) = f'[{exp_df.iloc[0][0]}]'
+            exp_df = ', '.join((exp_df.iloc[0]))
+
+        #print output
+        print('__________________________')
+        print(item)
+        print(obstimes)   
+        print(f'Exp: {exp_df}')
+        if len(focus_log) >= 10:
+            print(f'Focus: {focus}')
+            print(f'{focus_log}')
+        else:
+            print(f'Focus: {focus} {focus_log}')
+        if not args.bypass:
+            print(f'Weather: {obsdate_weather}')
+            print(f'Comments: {comment}')
+        else:
+            print('Comments:')
+        #time.sleep(2)
+    print('__________________________')
+    print('\nNote:')
+    if len(active_ccds) != 4:
+        print(f'\n**MISSING CCD**Exposure times are for CCDs {active_ccds} respectively.\n')
+    for error in error_message:
+        print(f'{error}\n')
+    #if args.obsdate is not None:
+    #    print('Please specify the CCD used for ag separately.')
+    print('Optional argument --jd can be provided to show times in JD.\n')
+
+for obsdate, obsdate_weather, comment in zip(obsdates, weather, comments):
+    print_obslog(obsdate, obsdate_weather, comment, ip['shine'])
+#一番短い露光時間のCCDの観測開始時間が1分遅れてしまうバグがある m秒のずれでCCD間で59秒と00秒になってしまうことが判明したのでseconds == 59の場合、(timdelta(seconds=1))で解決
---
+++ past_observation_crawler.py
@@ -0,0 +1,170 @@
+#import csv
+from datetime import datetime, timedelta
+import json
+#import numpy as np
+import os
+#import pandas as pd
+import requests
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+
+def obs(obsdate):
+    year = 2000 + int(obsdate[0:2])
+    month = int(obsdate[2:4])
+    day = int(obsdate[4:6])
+    return {'year':year,'month':month,'day':day}
+
+def replace_header(df):
+    new_header = df.iloc[0] #grab the first row for the header
+    df = df[1:] #get dataframe without the first row
+    df.columns = new_header 
+    return df
+
+def replace_index(df):
+    return df.set_index(df['star_id'])
+
+def adjust_name(str:str):
+    str = str.lower()
+    if 'toi0' in str:
+        str = str.replace('toi0', 'toi')
+    if '.01' in str:
+        str = str.replace('.01', '')
+    if '.1' in str:
+        str = str.replace('.1', '')
+    str = str.replace('-','')
+    return str
+
+def adjust_date(str):
+    dt = datetime.strptime(str,'%x, %I:%M %p')
+    if dt.hour >= 0 and dt.hour <= 6:
+        dt += timedelta(days=-1)
+    return dt.strftime('%y%m%d')
+
+def adjust_string(str):
+    if len(str) == 0:
+        str = "None"
+    return str
+
+def find_obsdates(target, observations_df, targets_df):
+    observations_df = replace_header(observations_df)
+    targets_df = replace_header(targets_df)
+    targets_df['name'] = [adjust_name(s) for s in targets_df['name']]
+    try:
+        star_id = targets_df[targets_df['name'].str.contains(adjust_name(target))]['id'].iloc[0]
+    except IndexError:
+        print(f'No previous observation for {target} is found. \nTarget could also be registered under a different name.\n(eg. TOI-XXXX and TOI0XXXX)')
+        sys.exit(1) 
+    past_observation = observations_df[observations_df['star_id'] == star_id]
+
+    obsdates = list(past_observation['start_time'])
+    obsdates = [adjust_date(x) for x in obsdates if int(adjust_date(x)) >= 180414]
+    print('_____________________________________________')
+    if len(obsdates) == 0:
+        print(f'No previous observation for {target} is found. \nTarget could also be registered under a different name.\n(eg. TOI-XXXX and TOI0XXXX)')
+        sys.exit(1)
+    else:
+        print(f'{len(obsdates)} observation(s) for {target} are found on: (yymmdd)\n')
+
+    print(obsdates)
+
+    print('\nIf no obslogs are returned with the above dates, this may be due to the observation date wrongly entered on MuSCAT2 wiki.\
+           \neg: Observations past midnight are registered with the date before midnight.\
+           \nIn that case, obslog can be found on the next day. [ie: /obs_log_formatter.py --obsdate=221114 -> 221115]')
+    print('_____________________________________________')
+    weather = list(past_observation['weather'])
+    comments = list(past_observation['comments'])
+
+    choice = input("Download all [y/N]: ").lower()
+    if choice in ['y', 'ye', 'yes']:
+        pass
+    elif choice in ['n', 'no']:
+        dl = input("Obsdate(s) to download in yymmdd [int or list(int)]: ")
+        if type(dl) == int:
+            dl = [dl]
+        dl_index = [obsdates.index(str(x)) for x in obsdates if x in dl]
+        obsdates = [x for x in obsdates if x in dl]
+        weather = [adjust_string(x) for x in weather if weather.index(x) in dl_index]
+        comments = [adjust_string(x) for x in comments if comments.index(x) in dl_index]
+        if len(weather) == 0: #accounts for obslog without weather data
+            weather = ["" for x in dl_index]
+        if len(comments) == 0: #accounts for obslog without comments
+            comments = ["" for x in dl_index]
+    return obsdates, weather, comments
+
+def find_weather_and_comments(target, observations_df, targets_df, obsdate, start_time, end_time, payload):
+    observations_df = replace_header(observations_df)
+    targets_df = replace_header(targets_df)
+    targets_df['wiki_name'] = targets_df['name']
+    targets_df['name'] = [adjust_name(s) for s in targets_df['name']]
+    try:
+        star_id = targets_df[targets_df['name'].str.contains(adjust_name(target))]['id'].iloc[0]
+    except IndexError:
+        return "","Target does not seem to be registered on wiki.", "", None
+    
+    past_observation = observations_df[observations_df['star_id'] == star_id]
+    obsdates = list(past_observation['start_time'])
+    past_observation['start_time'] = [adjust_date(x) for x in obsdates]
+    try:
+        weather = past_observation[past_observation['start_time'] == obsdate]['weather'].iloc[0]
+        comments = past_observation[past_observation['start_time'] == obsdate]['comments'].iloc[0]
+    except IndexError:
+        #ここでtarget観測を登録するか聞く（各天体についてのループの中でここに辿り着いてるから、ここでinputを促せばそのまま登録できる？）
+        date_for_view = datetime(obs(obsdate)['year'],obs(obsdate)['month'],obs(obsdate)['day']).strftime('%B %d, %Y')
+        print('\n____NEW OBSERVATION__________________________')
+        print(f'\nThe following observation of {target} has not been recorded on wiki yet.\n')
+        choice = input(f'Register {target}\'s observation on {date_for_view} to wiki [y/N]: ').lower()
+        print('_____________________________________________\n')
+        if choice in ['y', 'ye', 'yes']:
+            try:
+                with requests.Session() as s:
+                    weather = input('Weather: ')
+                    ag = input('CCD for ag [0|1|2|3]: ')
+                    focus_options = ["In focus,", "On focus,", "Slightly defocused,", "Defocused,", "Heavily defocused,"]
+                    focus_index = input('Focus [0:in focus|1:on focus|2:slightly defocused|3:defocused|4:heavily defocused]: ')
+                    focus = focus_options[int(focus_index)]
+                    comments = input('Comments: ')
+                    observers = input('Observers: ')
+                    obsdata = {
+                        'star_id': star_id,
+                        'start_time[year]': start_time.year,
+                        'start_time[month]': start_time.month,
+                        'start_time[day]': start_time.day,
+                        'start_time[hour]':start_time.hour,
+                        'start_time[minute]':start_time.minute,
+                        'end_time[year]': end_time.year,
+                        'end_time[month]': end_time.month,
+                        'end_time[day]': end_time.day,
+                        'end_time[hour]':end_time.hour,
+                        'end_time[minute]':end_time.minute,
+                        'telescope' : 'MuSCAT2',
+                        'observer': observers,
+                        'weather': weather,
+                        'seeing': "",
+                        'exposure_time': "",
+                        'bias':1,
+                        'flats':1,
+                        'lightcurve':1,
+                        'quicklook':1,
+                        'comments': f'Ag with CCD{ag}. {comments}',
+                    }
+                    #print(obsdata)
+                    print('_____________________________________________\n')
+                    print('Registering... (takes about 10-15 seconds)')
+                    print('_____________________________________________')
+                    p = s.post('http://research.iac.es/proyecto/muscat/users/login', data=payload)
+                    #print(p.status_code)
+                    registration = s.post('http://research.iac.es/proyecto/muscat/observations/add', data=obsdata)
+                    if registration.status_code == 404:
+                        print(f'Registration failed. Please check your word count in comments. ({target}, {date_for_view})')
+                    else:
+                        print(f'\nRegistration complete! ({target}, {date_for_view})\n')
+                        print(f'Check at http://research.iac.es/proyecto/muscat/stars/view/{star_id}\n')
+                    return weather, comments, focus, int(ag)
+            except requests.exceptions.ConnectionError:
+                print('Bad connection with the server. A simple retry should fix the issue. Make sure that you are connected to VPN.')
+                sys.exit(1)
+        elif choice in ['n', 'no']:
+            return "","Observation does not seem to be registered on wiki yet.","", None
+    return weather, comments, "", None

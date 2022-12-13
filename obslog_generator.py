8344271eb5ef3ba9950ab3b578999d292261d39e
ykawai6581 <yugo6581@g.ecc.u-tokyo.ac.jp>
2022-12-13 13:28:21 +0900

    fix bypass

--- obslog_generator.py
+++ obslog_generator.py
@@ -86,6 +86,12 @@ else: #日付指定をしている場合(天体指定の有無に関わらず)we
     obsdates = [str(args.obsdate)]
     weather, comments = [""],[""]
 
+def shorten_url(url, url_shortener):
+    try:
+        url = url_shortener.tinyurl.short(url)
+    except pyshorteners.exceptions.ShorteningErrorException:
+        url = "Generation failed. Please try again"
+
 #returns the observation date according to input
 def obs(obsdate):
     year = 2000 + int(obsdate[0:2])
@@ -283,10 +289,10 @@ def print_obslog(obsdate, obsdate_weather, comment, ip):
                 max_humidity = "Observation too short for archival data"
                 min_humidity = ""
             url_shortener = pyshorteners.Shortener()
-            humidity_plot = url_shortener.tinyurl.short(humidity_plot)
+            humidity_plot = shorten_url(humidity_plot)
 
             if not args.bypass:
-                altitude_plot = url_shortener.tinyurl.short(altitude_plot)
+                altitude_plot = shorten_url(altitude_plot)
             else:
                 url = s.get(f'https://exofop.ipac.caltech.edu/tess/target.php?id={adjust_name(item)}&json')
                 text = url.text
@@ -299,7 +305,7 @@ def print_obslog(obsdate, obsdate_weather, comment, ip):
                     jd = Time(start_time.strftime('%Y-%m-%d %T'),format='iso',out_subfmt='date_hm').jd
                     warning = " (THE GREEN SECTION INDICATES OBSERVATION TIME NOT TRANSIT TIME)"
                     altitude_plot = f'https://astro.swarthmore.edu/telescope/tess-secure/plot_airmass.cgi?observatory_string=28.3%3B-16.5097%3BAtlantic%2FCanary%3BMuSCAT2%201.52m%20at%20Teide%20Observatory%3BMuSCAT2%201.52m&observatory_latitude=28.3&observatory_longitude=-16.5097&target={item}{warning}&ra={ra}&dec={dec}&timezone=Atlantic/Canary&jd={jd}&jd_start={obslog[2][0]}&jd_end={obslog[2].iloc[-1]}&use_utc=0&max_airmass=2.4'
-                    altitude_plot = url_shortener.tinyurl.short(altitude_plot)
+                    altitude_plot = shorten_url(altitude_plot)
                 except json.decoder.JSONDecodeError:
                     altitude_plot = "No altitude plot found"
 
